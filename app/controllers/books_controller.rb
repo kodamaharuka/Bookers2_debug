1bd743deb0c5bba4d2edcbefd9950af7bfdacc14
kodamaharuka <ec2-user@ip-172-31-35-174.ec2.internal>
2021-04-06 08:17:30 +0000

    kadai3 fix

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,11 @@
 class BooksController < ApplicationController
+  before_action :authenticate_user!
   before_action :ensure_correct_user, only: [:edit, :update, :destroy]
 
   def show
     @book = Book.find(params[:id])
     @book_new = Book.new
+    @book_comment = BookComment.new
   end
 
   def index
@@ -54,3 +56,4 @@ class BooksController < ApplicationController
       end
   end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
-  resources :users,only: [:show,:index,:edit,:update]
-  resources :books
   get '/home/about', to: 'homes#about'
   
+  resources :books do
+   resource :favorites, only: [:create, :destroy]
+   resources :book_comments, only: [:create, :destroy]
+  end
+  resources :users, only: [:index, :show, :edit, :update]
 end
