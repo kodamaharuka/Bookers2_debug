e25e1cc367a765006624d0933aec564034a616ab
kodamaharuka <ec2-user@ip-172-31-35-174.ec2.internal>
2021-04-06 11:39:47 +0000

    kadai4

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,15 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get '/home/about', to: 'homes#about'
   
-  resources :books do
+  resources :books, only: [:index, :show, :edit, :create, :destroy, :update] do
    resource :favorites, only: [:create, :destroy]
    resources :book_comments, only: [:create, :destroy]
   end
-  resources :users, only: [:index, :show, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update] do
+   resource :relationships, only: [:create, :destroy]
+   get 'followings' => 'relationships#followings', as: 'followings'
+   get 'followers' => 'relationships#followers', as: 'followers'
+  end
+ 
 end
+
